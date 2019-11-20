05bacec7bf972d4cb3eeab2d52b720c43ab7d8c4
Sami Jouppila <sami.jouppila@gmail.com>
2019-11-20 13:40:02 +0200

    Kasvojentunnistin.kv file and main program loop for ui added

--- gui/gui.py
+++ gui/gui.py
@@ -1,10 +1,37 @@
-from kivy.app import App
-from kivy.uix.button import Button
+# -*- coding: utf-8 -*-
+from kivy.config import Config
+from kivy.properties import ObjectProperty
+from kivy.clock import Clock
 
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Menu(BoxLayout):
+    loginButton = ObjectProperty(None)
+    logoutButton = ObjectProperty(None)
+    responseButton = ObjectProperty(None)
+
+    def initializeMenu(self):
+        self.remove_widget(self.responseButton)
+
+    def mainMenu(self):
+        self.remove_widget(self.responseButton)
+        self.add_widget(self.loginButton)
+        self.add_widget(self.logoutButton)
+
+    def waitingResponse(self):
+        self.add_widget(self.responseButton)
+        self.remove_widget(self.loginButton)
+        self.remove_widget(self.logoutButton)
+        Clock.schedule_once(lambda dt: self.mainMenu(), 2)
 
 class KasvojentunnistinApp(App):
     def build(self):
-        return Button(text="Kirjaudu sisään", pos=(100,350), size_hint = (.25, .18))
+        app = Menu()
+        app.initializeMenu()
+        return app
 
 if __name__ == '__main__':
     KasvojentunnistinApp().run()
